19f027af0b88ff22344db33de7ea39b53f0f2bab
williykz <williamsrtmartin@gmail.com>
2026-05-07 19:14:54 -0400

    Proyecto inicial 1.1

--- servicio-libros/servicio-libros/src/main/java/com/example/servicio_libros/service/LibroService.java
+++ servicio-libros/servicio-libros/src/main/java/com/example/servicio_libros/service/LibroService.java
@@ -1,4 +1,47 @@
 package com.example.servicio_libros.service;
 
+import com.example.servicio_libros.model.Libro;
+import com.example.servicio_libros.repository.LibroRepository;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Service;
+
+@Service
 public class LibroService {
+    private final LibroRepository libroRepository;
+
+    public LibroService(LibroRepository libroRepository){
+        this.libroRepository =libroRepository;
+
+    }
+    public List<Libro> listarLibro(){
+        return libroRepository.findAll();
+
+
+    }
+    public Libro buscarPorId(@NonNull Long id){
+        return libroRepository.findById(id.orElse(null));
+    }
+    public Libro guardarLibro(Libro libro){
+        return libroRepository.save(libro);
+    }
+    public Libro actualizarLibro(Long id, Libro libro){
+        Libro libroExistente = buscarPorId(id);
+
+        if (libroExistente == null){
+            return  null;
+        }
+
+        libroExistente.setTitulo(libro.getTitulo());
+        libroExistente.setAutor((libro.getAutor()));
+        libroExistente.setIsbn((libro.getIsbn()));
+        libroExistente.setCategoria((Libro.getCategoria()));
+        libroExistente.setPrecio(libro.getPrecio());
+        libroExistente.setAnioPublicacion(libro.getAnioPublicacion());
+        libroExistente.setEditorialId(libro.getEditorialId());
+        libroExistente.setActivo(libro.getActivo());
+        return libroRepository.save(libroExistente);
+
+    }
+
+
 }
