e4496585d2d193bd66048536310f02e3f5ac5ccf
williykz <116925489+williykz@users.noreply.github.com>
2026-05-10 02:48:22 -0400

    Proyecto terminado 5 micro servicios

--- servicio-libros/servicio-libros/src/main/java/com/example/servicio_libros/model/Libro.java
+++ servicio-libros/servicio-libros/src/main/java/com/example/servicio_libros/model/Libro.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Generated;
 import lombok.NoArgsConstructor;
 
 @Entity
